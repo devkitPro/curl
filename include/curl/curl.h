a097f66e7c6369ed651c8fb2f09d832b6e505d73
EmreTech <50607143+EmreTech@users.noreply.github.com>
2021-04-29 02:06:43 -0700

    Fixed build issue if sys/select.h wasn't included before curl (#1)

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -75,7 +75,8 @@
     defined(__minix) || defined(__SYMBIAN32__) || defined(__INTEGRITY) || \
     defined(ANDROID) || defined(__ANDROID__) || defined(__OpenBSD__) || \
     defined(__CYGWIN__) || \
-   (defined(__FreeBSD_version) && (__FreeBSD_version < 800000))
+   (defined(__FreeBSD_version) && (__FreeBSD_version < 800000)) || \
+   defined(__SWITCH__)
 #include <sys/select.h>
 #endif
 
